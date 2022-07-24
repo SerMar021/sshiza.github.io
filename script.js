70957646e7af2f60870d8d478ba42687187bc143
SerMar021 <67406940+SerMar021@users.noreply.github.com>
2022-07-25 00:57:39 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function soundClick() {
+    var audio = new Audio(); // Создаём новый элемент Audio
+    audio.src = 'ui.mp3'; // Указываем путь к звуку "клика"
+    audio.autoplay = true; // Автоматически запускаем
+  }
+  
